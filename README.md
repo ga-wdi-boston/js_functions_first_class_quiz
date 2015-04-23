835321997a47d1f4cd73134c5c90a38189e65e35
tsyan <anna.tsykalova@gmail.com>
2015-04-23 15:08:15 -0400

    demo of quiz, incomplete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# quiz_template
+# Quiz: Javascript Fundamentals
+
+Directions: I will fill this in later. Remind students that they can execute the code.
+
+<!-- budget about 5 minutes per question -->
+<!-- quiz questions should match objectives -->
---
+++ quiz.js
@@ -0,0 +1,33 @@
+// Quiz: Javascript Fundamentals
+
+
+// Question 1
+
+var pets = ["fido", "sparky", "fluffy"];
+
+// Write code to change the last entry in this array from "fluffy" to "spike". Do not modify the code above.
+
+// your code goes here! student's answer might look like...
+pets.pop();
+pets.push("spike");
+
+/////////////////////////////////////////////////////////
+
+
+
+
+// Question 2
+
+var 90s_bands = ["nirvana", "jimmy eat world", "spice girls"];
+
+// Write a for loop that goes through the array above and returns an array with the words "is awesome" at the end of each item. Do not use console.log in your solution. So the loop should return ["nirvana is awesome", "jimmy eat world is awesome", "spice girls is awesome"].
+
+
+/////////////////////////////////////////////////////////
+
+// Question 3
+
+
+// Question 4
+
+
