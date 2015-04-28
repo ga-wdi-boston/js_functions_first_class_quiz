dbdbdf1efbcdfa37fe16c186fbba9f701644045f
cwilbur <charlton.wilbur@generalassemb.ly>
2015-04-28 17:03:41 -0400

    initial draft

--- quiz.js
+++ quiz.js
@@ -1,45 +1,105 @@
-// Quiz: Topic Goes Here
-
-// quiz questions should match objectives
-// keep it simple
-// be very specific in your directions
-// 4 questions = about 20 minutes for students to complete
-// replace the example questions with your own
-
+// Quiz: Javascript Functions are First Class
 
 // Question 1
 
-var pets = ["fido", "sparky", "fluffy"];
+// A fellow student shows you this code.  When he runs it, he expects it to
+// wait three seconds, then write "Ding!" to the console.  Instead, it writes
+// "Ding!" immediately.  Find the bug and fix it.
 
-// Write code to change the last entry in this array from "fluffy" to "spike". Do not modify the code above.
+function writeDing() {
+  console.log('Ding!');
+}
 
-// student's answer might look like...
-pets.pop();
-pets.push("spike");
-
-
-
-/////////////////////////////////////////////////////////
+var dingHandle = setTimeout (writeDing(), 3000);
 
 // Question 2
 
-var 90s_bands = ["nirvana", "jimmy eat world", "spice girls"];
+// Javascript has a built-in sort function, but you need to tell it how to
+// compare the things you want to sort.
+//
+// You do this by passing it a function that takes two arguments,
+// which we'll call A and B.
+//
+// This function should return
+//   -1 if A should come before B,
+//   0 if they compare equal,
+//   and 1 if B should come before A.
+//
+// Write a function according to this specification so that the following code
+// sorts the words from shortest to longest.
+//
+// Remember that if x is a variable containing a string, you can find its
+// length with the expression x.length.
 
-// Write a for loop that goes through the array above and returns an array with the words "is awesome" at the end of each item. Do not use console.log in your solution. So the loop should return ["nirvana is awesome", "jimmy eat world is awesome", "spice girls is awesome"].
+var words = ['short', 'medium', 'long', 'interminable', 'lengthy'];
 
+var byLength = function (A, B) {
+  // your code here
+};
 
+var sortedWords = words.sort(byLength);
 
+// testing for correct answer
 
-/////////////////////////////////////////////////////////
+var assert = require('assert');
+try {
+  assert.deepEqual(sortedWords, ['long', 'short', 'medium', 'lengthy', 'interminable']);
+  console.log("Answer to question 2 is correct");
+}
+catch (e) {
+  console.log("Answer to question 2 isn't correct yet.");
+}
 
 // Question 3
+//
+// What change would you make to your comparison function so that it will sort
+// the words from longest to shortest?  Write it as an anomymous function.
 
+var sortedWords = words.sort(function (A, B){
+  // your code here
+});
 
-
-
-/////////////////////////////////////////////////////////
+try {
+  assert.deepEqual(sortedWords, ['interminable', 'lengthy', 'medium', 'short', 'long']);
+  console.log("Answer to question 3 is correct");
+}
+catch (e) {
+  console.log("Answer to question 3 isn't correct yet.");
+}
 
 // Question 4
 
+// You have the following code to calculate the nth power of a number m:
 
+function power(m, n) {
+  var result = 1;
 
+  for (var i = 1; i < n; i++) {
+    result *= m;
+  }
+
+  return result;
+}
+
+// Write a function generator called makePower based on this code so
+// that the following code works.  The function you return should take
+// a number as an argument and return that number raised to the power
+// you specified in the makePower call.
+
+function makePower(power) {
+  // your code here
+}
+
+var power2 = makePower(2);
+var power3 = makePower(3);
+
+try {
+  assert.strictEqual(power2(4), 16);
+  assert.strictEqual(power2(3), 9);
+  assert.strictEqual(power3(2), 8);
+  assert.strictEqual(power3(3), 27);
+  console.log("Answer to question 4 is correct");
+}
+catch (e) {
+  console.log("Answer to question 4 isn't correct yet.");
+}
